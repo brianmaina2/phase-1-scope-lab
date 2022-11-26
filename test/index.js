99220657ba11775f3aab74c4a2947b5a04888167
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-27 00:42:52 +0300

    commit tree 23e8f69831fe5a923e2ad14d27ba2f708005b50c

---
+++ test/index.js
@@ -0,0 +1,19 @@
+
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer ='something';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'victor';
+}
